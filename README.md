4b197a31988b135f8e2a484653bcadf1d8ecc0b0
Richard <rzhang606@gmail.com>
2020-08-12 17:51:42 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Phonebook Backend - fullstackopen.com course
+
+Base Endpoint: https://serene-sands-63313.herokuapp.com/
+
+- /info : information
+- /api/persons/[id] : people or specific person
---
+++ build/precache-manifest.9e779d2aff968be4e0934b9cb1bef024.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ead9763a398aae2b831fc9782bcbdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82399ab1406ef6ba54ea",
+    "url": "/static/css/main.758ba04a.chunk.css"
+  },
+  {
+    "revision": "d3a98a2dc6b5704918c9",
+    "url": "/static/js/2.a92b001b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a92b001b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82399ab1406ef6ba54ea",
+    "url": "/static/js/main.b6f7fb6f.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
