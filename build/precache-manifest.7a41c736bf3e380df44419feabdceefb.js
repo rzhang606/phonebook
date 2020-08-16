727f505d30034a5586fded14cc6e53afaf8b85eb
Richard <rzhang606@gmail.com>
2020-08-16 15:35:59 -0700

    deploy

---
+++ build/precache-manifest.7a41c736bf3e380df44419feabdceefb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201deffe19b4bdd55a7a5c1316da481c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60ac7fccdaeff0c94144",
+    "url": "/static/css/main.758ba04a.chunk.css"
+  },
+  {
+    "revision": "a2de0c972fbbc380ae04",
+    "url": "/static/js/2.b26ff8e3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b26ff8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ac7fccdaeff0c94144",
+    "url": "/static/js/main.5088e7bc.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
